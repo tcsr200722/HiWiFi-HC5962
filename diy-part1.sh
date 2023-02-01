9f4bc12a28a8ef41bd43392e7a2ae3fd31868a2c
MikuMaxZ <73537271+MikuMaxZ@users.noreply.github.com>
2023-02-01 15:37:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus
